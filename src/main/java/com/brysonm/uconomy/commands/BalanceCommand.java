6ecd438b4f1f9f5de696df1fc58351558073cd59
bm <bryson@brysonm.com>
2014-02-19 20:50:51 -0500

    Added admin balance command

--- src/main/java/com/brysonm/uconomy/commands/BalanceCommand.java
+++ src/main/java/com/brysonm/uconomy/commands/BalanceCommand.java
@@ -1,6 +1,7 @@
 package com.brysonm.uconomy.commands;
 
 import com.brysonm.uconomy.BalanceUtils;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -10,6 +11,7 @@ import org.bukkit.entity.Player;
 public class BalanceCommand implements CommandExecutor {
 
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+
         if(sender instanceof Player) {
 
             Player player = (Player) sender;
@@ -17,6 +19,26 @@ public class BalanceCommand implements CommandExecutor {
             if(!player.hasPermission("uconomy." + cmd.getName().toLowerCase())) {
 
                 player.sendMessage(ChatColor.RED + "You do not have permission to execute this command!");
+
+                return true;
+
+            }
+
+            if(args.length == 1 && player.hasPermission("uconomy.admin")) {
+
+                Player target = Bukkit.getPlayer(args[1]);
+
+                if(target == null) {
+
+                    player.sendMessage(ChatColor.RED + "That player does not exist!");
+
+                    return true;
+
+                }
+
+                double balance = BalanceUtils.getBalance(target);
+
+                player.sendMessage(ChatColor.GRAY + target.getName() + " has " + balance + " gold in their bank account.");
 
                 return true;
 
